5a20f8a46f6de64b9294569e9512d84977fe5241
Mariia Koliada <mariia.koliada@powercoders.org>
2024-10-18 23:39:45 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-console.log('This is a my first chrome extention!');
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.create({ url: chrome.runtime.getURL("hello.html") });
+});
+document.addEventListener('dblclick', (event) => {
+    const selectedText = window.getSelection().toString().trim();
+    if (selectedText.length > 0 && selectedText.split(' ').length === 1) {
+      chrome.runtime.sendMessage({ action: "storeWord", word: selectedText });
+    }
+  });
